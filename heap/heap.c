b13bb2ba64d8e980a6a9305eee0a59f12665062b
alex <xandhiller@gmail.com>
2020-11-05 12:02:52 +1100

    Some implementation of the heap methods: init expand order_rel next_empty peek

--- heap/heap.c
+++ heap/heap.c
@@ -5,6 +5,41 @@
  *    Implementation of the heap data structure, based on arrays, in C.
  */
 
+#include "heap.h"
+
+/* Initialise the heap by calling init on the dynamic array. */
+void hp_init(heap_t* hp) {
+    da_init(hp->array);
+}
+
+
+bool hp_expand(heap_t* hp) {
+    return da_expand(hp);
+}
+
+
+int8_t hp_order_rel(DATA_TYPE a, DATA_TYPE b) {
+    if (a > b) {
+        return A_GREATER;
+    }
+    else if (b > a) {
+        return B_GREATER;
+    }
+    else {
+        return A_EQUAL_B;
+    }
+}
+
+
+uint64_t hp_next_empty(heap_t* hp) {
+    return hp->array->nb_vals;
+}
+
+
+DATA_TYPE hp_peek(uint64_t location, heap_t* hp) {
+    return hp->array->data[location];
+}
+
 
 
 int main (int argc, char *argv[]) {
--- heap/heap.h
+++ heap/heap.h
@@ -3,48 +3,70 @@
 
 
 /* Defines & Enumerations */
-
+#define A_GREATER   1 
+#define B_GREATER  -1
+#define A_EQUAL_B   0
 
 /* Libraries */
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include "../arrays/dynamic_arrays.h"
 
 
 /* Structures */
 struct heap {
-    dynamic_array_t* data;
+    dynamic_array_t* array;
 };
 typedef struct heap heap_t;
 
 
-/* Needs to be like da_expand, but always expand in powers of two. */
-void hp_expand(void);
+/* Initialise the heap, init the dynamic array. */
+void hp_init(heap_t* hp);
+
+
+/* Must always expand in powers of two -- da_expand has been altered to only
+ * behave this way. */
+bool hp_expand(heap_t* hp);
+
+
+/* The order relation used to determine the ordering of the heap. Currently uses
+ * the '>' and '<' of the order relation. */
+int8_t hp_order_rel(DATA_TYPE a, DATA_TYPE b);
+
 
 /* Get the first index of the heap that is empty.  */
-uint64_t hp_next_empty(heap_t hp);
+uint64_t hp_next_empty(heap_t* hp);
+
 
 /* Returns the value at "location", without altering the heap.  */
-DATA_TYPE hp_peak(uint64_t location, heap_t* hp);
+DATA_TYPE hp_peek(uint64_t location, heap_t* hp);
+
 
 /* Each node has two children, so must indices must be returned as a pointer to 
  * an array of the index values.  */
 uint64_t* hp_get_children(void);
 
+
 /* Returns the index of the parent for the given node.  */
 uint64_t hp_get_parent(uint64_t index, heap_t hp);
 
 
+/* Add an element to the heap. */
 void hp_add(void);
+
 
 void hp_bubble_up(void);
 
+
 void hp_bubble_down(void);
+
 
 void hp_pop_by_val(void);
 
+
 void hp_pop_by_index(void);
+
+
+void hp_display(heap_t* hp);
 
