0d2f5d5a62a92e0c896e781e1bc0dfb12f0e5fac
alex <xandhiller@gmail.com>
2020-11-07 16:32:22 +1100

    Some descriptions of the fns written into header.

--- union_find/union_find.h
+++ union_find/union_find.h
@@ -31,9 +31,30 @@ typedef struct union_find union_find_t;
 /******************************************************************************/
 
 
+/* Initialisation of the union find, sets the number of vertices, 
+ * allocates the array and fills it with a bijective identity mapping for each
+ * node. */
 void uf_init(union_find_t* uf, uint64_t nb_elems);
+
+
+/* Returns the index of the parent of the given vertex, a. */
 uint64_t uf_get_parent(uint64_t a, union_find_t* uf);
+
+
+/* Tells the user (via True or False) whether the given node is a root node, 
+ * i.e. whether it points to itself. */
 bool uf_is_root(uint64_t a, union_find_t* uf);
+
+
+/* Given a vertex, the index of the root node is returned.  */
 uint64_t ug_get_root(uint64_t a, union_find_t* uf);
+
+
+/* Returns a boolean about whether two nodes have the same root, which can 
+ * be interpretted as whether two elems are in the same group. */
 bool uf_find(uint64_t a, uint64_t b, union_find_t* uf);
+
+
+/* Makes the elements a and b have the same root node, which can be interpretted 
+ * as unifying the elements into the same set. */
 void uf_union(uint64_t a, uint64_t b, union_find_t* uf);
